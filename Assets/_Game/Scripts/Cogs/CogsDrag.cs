3963a0e03056cd66edff39b05e6214466ae78ac6
Samundra <wdymsamundra@gmail.com>
2026-04-15 11:32:42 +0545

    Drag System Refined: changed from click and drag to hold system

--- Assets/_Game/Scripts/Cogs/CogsDrag.cs
+++ Assets/_Game/Scripts/Cogs/CogsDrag.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,29 +13,28 @@ public class CogsDrag : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
-            
-           GameObject clickedObject = hit.collider.gameObject;
-            
-           if (clickedObject.tag == "Cogs")
-           {
-                onDrag = !onDrag;
-           }
-           else
-           {
-               onDrag = !onDrag;
-           }
-        }
         Hold();
+    }
+
+    private void OnMouseDown()
+    {
+        
+        if (gameObject.CompareTag("Cogs"))
+        {
+            Debug.Log("OnMouseDown");
+            zDepth = Camera.main.WorldToScreenPoint(transform.position).z;
+            offset = transform.position - GetMouseWorldPos();
+            onDrag = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        onDrag = false;
     }
 
     private void Hold()
     {
-        zDepth = Camera.main.WorldToScreenPoint(transform.position).z;
-        offset = transform.position - GetMouseWorldPos();
         if (onDrag)
         {
             this.transform.position = GetMouseWorldPos();
@@ -43,8 +43,6 @@ public class CogsDrag : MonoBehaviour
 
     private Vector3 GetMouseWorldPos()
     {
-
-        
         Vector3 mouseScreenPos = new Vector3(
             Input.mousePosition.x,
             Input.mousePosition.y,
