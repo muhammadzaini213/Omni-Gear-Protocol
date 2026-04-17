9d144ebe3c680840e471d759fc9d4a48f8f60a88
KangkungGoreng <syabaninurzain@gmail.com>
2026-04-17 15:17:06 +0800

    Added Death Animation & Refine Jump
    Added Death
    Shorten Jump Animation from 0:20 to 0:15
    fix some issue

--- Assets/_Game/Scripts/Entity/Player/PlayerHealth.cs
+++ Assets/_Game/Scripts/Entity/Player/PlayerHealth.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class PlayerHealth : Health
 {
+    private Animator animator;
+    private Rigidbody2D rb;
+    private PlayerMove playerMove;
+    void Awake()
+    {
+        animator = GetComponentInParent<Animator>();
+        rb = GetComponentInParent<Rigidbody2D>();
+        playerMove = GetComponentInParent<PlayerMove>();
+    }
     protected override void Start()
     {
         base.Start();
@@ -11,6 +20,16 @@ public class PlayerHealth : Health
     private void HandlePlayerDeath()
     {
         Debug.Log("Player died! Show Game Over.");
+        animator.SetTrigger("Death");
+
+        if (playerMove != null)
+        {
+            playerMove.enabled = false;
+        }
+        if (rb != null)
+        {
+            rb.velocity = Vector2.zero;
+        }
     }
 
     public override void TakeDamage(int amount)
