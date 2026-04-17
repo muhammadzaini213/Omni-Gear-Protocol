b3d94ede56ae9201de707355b66e950010107386
Samundra <wdymsamundra@gmail.com>
2026-04-17 22:01:44 +0545

    changes

--- Assets/_Game/Scripts/Entity/Cogs/Cogs.cs
+++ Assets/_Game/Scripts/Entity/Cogs/Cogs.cs
@@ -6,14 +6,42 @@ namespace _Game.Scripts.Cogs
     {
         public CogsType cogType;
         public float rotateSpeed;
-        public bool reverseRotation;
+        public bool reverseDirection;
+        public bool isSnapped;
+        public CogSnapChannel snapChannel;
+        
 
         void Update() => RotateCogs();
 
         public void RotateCogs()
         {
-            float direction = reverseRotation == true ? -1 : 1;
+            if(!isSnapped) return;
+            
+            float direction = reverseDirection == true ? -1f: 1f;
             transform.Rotate(0f, 0f, direction * rotateSpeed * Time.deltaTime);
+        }
+
+        public void SnapNotify(GameObject obj, CogsType type)
+        {
+            isSnapped = true;
+        }
+
+        public void UnsnapNotify(GameObject obj, CogsType type)
+        {
+            isSnapped = false;
+        }
+        
+     
+        private void OnEnable()
+        {
+            snapChannel.OnCogSnapped += SnapNotify;
+            snapChannel.OnCogUnsnapped += UnsnapNotify;
+        }
+
+        private void OnDisable()
+        {
+            snapChannel.OnCogSnapped -= SnapNotify;
+            snapChannel.OnCogUnsnapped -= UnsnapNotify;
         }
     }
 
